9b4f8b0ce7b5a7732ab9022d566c0ad86c3bfd84
Skoresby <lea.calem@gmail.com>
2018-05-07 15:03:38 +0200

    ajout

--- src/launch/Myclass.java
+++ src/launch/Myclass.java
@@ -1,5 +1,15 @@
 package launch;
 
+import javax.swing.JFrame;
+
 public class Myclass {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Yo");
+		JFrame fenetre = new JFrame();
+		fenetre.setTitle("hello");
+		fenetre.setSize(1300,750);
+		fenetre.setVisible(true);
+	}
 
 }
