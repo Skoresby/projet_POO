a3e6abe9737411ad224ee72e8eaaf85e64d44e85
lan <daniel.teboul@laposte.net>
2018-06-12 20:10:36 +0200

    Implémentation en partie de la base grpahique pour triomino

--- src/launch/Main.java
+++ src/launch/Main.java
@@ -16,22 +16,23 @@ public class Main extends JFrame{
 		int indiceChoisi=-1;
 		int retourCoupPossible=-1;
 		int finJeu=-1;
-		
-		System.out.println("je suis dans le bon dossier!!!");
+		Pion[] hand = new Pion[10];
+		//System.out.println("je suis dans le bon dossier!!!");
 		Jeu jeu = new Jeu();
 		Joueur joueur[] = new Joueur[4];
 
-		Partie jeton = jeu.choix_jeu();
+		//Partie jeton = jeu.choix_jeu();
 		//Domino dom= new Domino();
 		
-		jeton.entrer_joueurs(joueur);
+		//jeton.entrer_joueurs(joueur);
 		
 		
-		jeton.afficher_base_jeu();
+		//jeton.afficher_base_jeu();
 
-		Pion[]tableau=jeton.init_tab();
-		Pion[][] plateau=jeton.init_plateau();
-		joueurAct=jeton.premier_a_jouer();		
+		//Pion[]tableau=jeton.init_tab();
+		//Pion[][] plateau=jeton.init_plateau();
+		triangle_m aff = new triangle_m();
+		/*joueurAct=jeton.premier_a_jouer();		
 		do
 		{
 			System.out.println("le joueur numero "+joueurAct+" commence");
@@ -42,7 +43,7 @@ public class Main extends JFrame{
 			//System.out.println("retour : coll="+(retourCoupPossible/10)+" nbre de tours a faire : "+(retourCoupPossible%10));
 		
 			finJeu=jeton.partie_finie(joueur);
-		}while(finJeu==0);
+		}while(finJeu==0);*/
 		
 	}
 
